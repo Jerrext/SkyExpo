6a3cf907c46562c93d2b13616983691bf7d7f210
Jerrext <113127858+Jerrext@users.noreply.github.com>
2023-01-13 22:06:58 +0300

    Fix animations and add styles for 'About'

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ const smoothCoef = 0.05;
 const smoothScroll = document.querySelector(".smooth-scroll");
 const smoothScrollBar = document.querySelector(".smooth-scrollbar");
 const bgImg = document.querySelector(".bg-img")
-const titleLeft = document.querySelector(".title-block-left")
+const titleRight = document.querySelector(".title-block-left")
+const animItemsIn = document.querySelectorAll('._anim-items'); // Появление блоков
+const animItemsOut = document.querySelectorAll('._anim-out')
 
 function onResize(e) {
   smoothScrollBar.style.height = smoothScroll.offsetHeight + "px";
@@ -18,21 +20,71 @@ let curY = window.scrollY;
 let y = window.scrollY;
 let dy;
 
+const blockIn = window.innerHeight * 0.9 // Координаты появления блоков относительно окна
+const blockOut = window.innerHeight * 0.2
+// console.log(window.innerHeight/15)
+
 function loop(now) {
   curY = window.scrollY;
   dy = curY - prevY;
   y = Math.abs(dy) < 1 ? curY : y + dy * smoothCoef;
-  // console.log(dy)
   prevY = y;
+
+  for (let i = 0; i < animItemsIn.length; i++) {
+    if (offset(animItemsIn[i]).top < blockIn){
+      animItemsIn[i].classList.add('_active');
+    } else {
+        if (!animItemsIn[i].classList.contains('_anim-no-hide')) {
+          animItemsIn[i].classList.remove('_active');
+        }
+    }
+  }
+
+  for (let i = 0; i < animItemsOut.length; i++) {
+    if ((offset(animItemsOut[i]).top < blockOut)) {
+      // console.log(cord(animItemsOut[i]))
+      animItemsOut[i].style.transform = `translateY(${cord(animItemsOut[i])/3 - window.innerHeight/15}px)`
+    }
+  }
+
   smoothScroll.style.transform = `translate3d(0,${-y}px,0)`;
   bgImg.style.transform = `translate3d(0,${(-y)/2}px,0)`
-  titleLeft.style.transform = `translateX(${qwe(titleLeft)/5 - 100}px)`
+  titleRight.style.transform = `translateX(${cord(titleRight)/5 - 100}px)`
 
+  // Карточки "О нас"
+
+  document.querySelectorAll(".bottom-in").forEach(item => {
+    item.style.transform = `translateY(${cord(item)/15 - 30}px)`
+  })
+
+  document.querySelectorAll(".left-in").forEach(item => {
+    item.style.transform = `translateX(${-cord(titleRight)/15}px)`
+  })
+
+  document.querySelectorAll(".right-in").forEach(item => {
+    item.style.transform = `translateX(${cord(titleRight)/15}px)`
+  })
+ 
+  // console.log(v)
+
+  // v.style.transform = `translateX(${(-y)/10}}px)`
+    
   requestAnimationFrame(loop);
 }
 requestAnimationFrame(loop);
 
-function qwe(elem) {
+function offset(el) {
+  const rect = el.getBoundingClientRect()
+      // scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      // scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return {
+      top: rect.top,
+      bottom: rect.bottom,
+      left: rect.left
+  }
+}
+
+function cord(elem) {
   return elem.getBoundingClientRect().top
 }
 
@@ -65,6 +117,10 @@ window.addEventListener("scroll", (e) => {
   } else {
     header.classList.remove("scrollNav")
   }
+
+  // console.log(cord(v))
+
+  
 })
 
 // Кнопка "Стрелка вниз"
@@ -80,40 +136,42 @@ arrowBanner.addEventListener("click", () => {
 
 // Появление текста
 
-const animItems = document.querySelectorAll('._anim-items');
-if (animItems.length > 0) {
-    window.addEventListener('scroll', animOnScroll);
-    function animOnScroll() {
-        for (let index = 0; index < animItems.length; index++) {
-            const animItem = animItems[index];
-            const animItemHeight = animItem.offsetHeight;
-            const animItemOffset = offset(animItem).top;
-            const animStart = 4;
-            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+// const animItems = document.querySelectorAll('._anim-items');
+// if (animItems.length > 0) {
+//     window.addEventListener('scroll', animOnScroll);
+//     function animOnScroll() {
+//         for (let i = 0; i < animItems.length; i++) {
+//             const animItem = animItems[i];
+//             const animItemHeight = animItems[i].offsetHeight;
+//             const animItemOffset = offset(animItem).top;
+//             const animStart = 4;
+//             let animItemPoint = window.innerHeight - animItemHeight / animStart;
 
-            if (animItemHeight > window.innerHeight) {
-                animItemPoint = window.innerHeight - window.innerHeight / animStart;
-            }
-            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-                animItem.classList.add('_active');
-            } else {
-                if (!animItem.classList.contains('_anim-no-hide')) {
-                    animItem.classList.remove('_active');
-                }
-            }
-        }
-    }
-    function offset(el) {
-        const rect = el.getBoundingClientRect(),
-            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        return {
-            top: rect.top + scrollTop,
-            left: rect.left + scrollLeft
-        }
-    }
-    animOnScroll();
-}
+//             if (animItemHeight > window.innerHeight) {
+//                 animItemPoint = window.innerHeight - window.innerHeight / animStart;
+//             }
+//             console.log(offset(titleRight).bottom)
+//             if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+//                 animItem.classList.add('_active');
+//             } else {
+//                 if (!animItem.classList.contains('_anim-no-hide')) {
+//                     animItem.classList.remove('_active');
+//                 }
+//             }
+//         }
+//     }
+//     function offset(el) {
+//         const rect = el.getBoundingClientRect()
+//             // scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+//             // scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//         return {
+//             top: rect.top,
+//             bottom: rect.bottom,
+//             left: rect.left
+//         }
+//     }
+//     animOnScroll();
+// }
 
 // window.addEventListener("scroll", () => {
 //   let a = window.scrollY
