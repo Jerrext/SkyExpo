3d077ea3d03b39a0f3773da121809f0a38fa9c97
Jerrext <113127858+Jerrext@users.noreply.github.com>
2023-02-21 02:56:22 +0300

    Add loading screen and popup windows, refactoring of smooth scrolling code and js animations, optimization of images and fonts

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,108 @@ const obs = {               // обсервер
   attributeFilter: ["class"]
 }
 
+// Загрузочный экран
+
+const loadingAnim = () => {
+  const item1 = document.querySelector('.loading__text')
+  const item2 = document.querySelector('.loading__text-bg')
+  const loading = document.querySelector('.loading')
+  const bannerElements = document.querySelector('.banner__text')
+  const hint = document.querySelector('.hint')
+  const fab = document.querySelector('.fab')
+  // const translateX = ((item1.parentElement.parentElement.clientWidth - item1.clientWidth) / 2)
+  console.log(item2.getBoundingClientRect().width)
+  setTimeout(() => {item1.style.width = `${item2.getBoundingClientRect().width}px`}, 300)
+  
+  // item2.style.transform = `translate(${translateX}px, 0)`
+  const time = 4200
+
+  document.documentElement.style.overflow = "hidden"
+  document.body.style.overflow = "hidden"
+
+  setTimeout(() => {
+    loading.style.opacity = '0'
+    document.documentElement.style.overflow = "hidden auto"
+    document.body.style.overflow = "hidden auto"
+    setTimeout(() => {
+      loading.style.display = 'none'
+    }, 1500)
+
+    hint.classList.add("_active")
+    fab.classList.add("_active")
+
+    let i = 0
+    const showbannerElements = setInterval(() => {
+      if (bannerElements.lastElementChild.classList.contains("_active")) {
+        clearInterval(showbannerElements)
+      } else {
+        bannerElements.children[i].classList.add("_active")
+        i++
+      }
+    }, 300)
+  }, time)
+}
+
+loadingAnim()
+
+
+
+
+// Создание элемента
+
+// const createElem = (tag, options, to) => {
+//   const elem = document.createElement(tag)
+
+//   Object.keys(options).forEach(item => {
+//       elem[item] = options[item]
+//   })
+//   to.appendChild(elem)
+
+//   return 
+// }
+
+// Модальные окна
+
+const callBtnHandler = (popupContainer) => {
+  const popup = document.querySelector(popupContainer)
+  const popupWrapper = document.querySelector(`${popupContainer} .popup__wrapper`)
+  const overlay = document.querySelector(`${popupContainer} .popup__overlay`)
+  const popupClose = document.querySelector(`${popupContainer} .popup__close`)
+
+  popup.style.display = "block"
+  document.documentElement.style.overflow = "hidden"
+  document.body.style.overflow = "hidden"
+
+  const popUpClose = () => {
+    popup.style.display = "none"
+    overlay.style.opacity = "0"
+    popupWrapper.style.opacity = "0"
+    popupWrapper.style.top = "calc(50% + 30px)"
+    document.documentElement.style.overflow = "hidden auto"
+    document.body.style.overflow = "hidden auto"
+  }
+
+  setTimeout(() => {
+    overlay.style.opacity = ".7"
+    popupWrapper.style.opacity = "1"
+    popupWrapper.style.top = "50%"
+  },0)
+
+  popup.addEventListener("click", (e) => {
+    if (e.target.className === "popup__overlay") {
+      popUpClose()
+    }
+  })
+
+  popupClose.addEventListener("click", () => {
+    popUpClose()
+  }) 
+}
+
+document.querySelector(".header .btn").addEventListener("click", () => callBtnHandler(".popup-call"))
+document.querySelector(".main__btn").addEventListener("click", () => callBtnHandler(".popup-call"))
+document.querySelector(".fab-buttons__link").addEventListener("click", () => callBtnHandler(".popup-email"))
+
 // Плавная прокрутка
 
 const smoothCoef = 0.05;
@@ -14,27 +116,17 @@ const bgBanner = document.querySelector(".bg-banner");
 const animItemsIn = document.querySelectorAll('._anim-items'); // Появление блоков
 const animItemsOut = document.querySelectorAll('._anim-out');
 
-function onResize(e) {
-  smoothScrollBar.style.height = smoothScroll.offsetHeight + "px";
-}
+// function onResize(e) {
+//   smoothScrollBar.style.height = smoothScroll.offsetHeight + "px";
+// }
 
-window.addEventListener("resize", onResize);
-onResize();
+// window.addEventListener("resize", onResize);
+// onResize();
 
-let prevY = window.scrollY;
-let curY = window.scrollY;
-let y = window.scrollY;
-let dy;
+// Появление блоков
 
-const blockIn = window.innerHeight * 0.9 // Координаты появления блоков относительно окна
-const blockOut = window.innerHeight * 0.15
-// console.log(window.innerHeight/15)
-
-function loop(now) {
-  curY = window.scrollY;
-  dy = curY - prevY;
-  y = Math.abs(dy) < 1 ? curY : y + dy * smoothCoef;
-  prevY = y;
+const showBlocks = () => {
+  const blockIn = window.innerHeight * 0.9 // Координаты появления блоков относительно окна
 
   for (let i = 0; i < animItemsIn.length; i++) {
     if (offset(animItemsIn[i]).top < blockIn){
@@ -45,15 +137,26 @@ function loop(now) {
         }
     }
   }
+}
 
-  for (let i = 0; i < animItemsOut.length; i++) {
-    if ((offset(animItemsOut[i]).top < blockOut)) {
-      // console.log(cord(animItemsOut[i]))
-      animItemsOut[i].style.transform = `translateY(${cord(animItemsOut[i])/3 - window.innerHeight/20}px)`
-    }
-  }
-  smoothScroll.style.transform = `translate3d(0,${-y}px,0)`;
-  bgBanner.style.transform = `translate3d(0,${y/3}px,0)`
+// Скрытие блоков
+
+// const hideBlocks = () => {
+//   const blockOut = window.innerHeight * 0.15
+
+//   for (let i = 0; i < animItemsOut.length; i++) {
+//     if ((offset(animItemsOut[i]).top < blockOut)) {
+//       // console.log(cord(animItemsOut[i]))
+//       animItemsOut[i].style.transform = `translateY(${cord(animItemsOut[i])/3 - window.innerHeight/20}px)`
+//     }
+//   }
+// }
+
+window.addEventListener("scroll", () => {
+  showBlocks()
+  // hideBlocks()
+
+  bgBanner.style.transform = `translate3d(0,${-cord(bgBanner)/2}px,0)`
   document.querySelectorAll('.bg-img').forEach(item => {
     item.style.transform = `translateY(${-cord(item)/3 - 300}px)`
   })
@@ -62,31 +165,74 @@ function loop(now) {
   // Анимация блоков
 
   document.querySelectorAll(".title-block-left").forEach(item => {
-    item.style.transform = `translateX(${cord(item)/5 - 100}px)`
+    if (cord(item) < (window.innerHeight + 100) && cord(item) > -300) {
+      item.style.transform = `translateX(${cord(item)/7}px)`
+    }
   })
 
   document.querySelectorAll(".bottom-in").forEach(item => {
     // if (item.classList.contains("bg-img")) {
     //   item.style.transform = `translateY(${cord(item) - 1350}px)`
     // }
-    item.style.transform = `translateY(${cord(item)/15 - 40}px)`
+    if (cord(item) < (window.innerHeight + 100) && cord(item) > -300) {
+      item.style.transform = `translateY(${cord(item)/15 - 40}px)`
+    }
   })
 
   document.querySelectorAll(".left-in").forEach(item => {
-    item.style.transform = `translateX(${-cord(item)/10 + 50}px)`
+    if (cord(item) < (window.innerHeight + 100) && cord(item) > -300) {
+      item.style.transform = `translateX(${-cord(item)/10 + 50}px)`
+    }
   })
 
   document.querySelectorAll(".right-in").forEach(item => {
-    item.style.transform = `translateX(${cord(item)/10 - 50}px)`
+    if (cord(item) < (window.innerHeight + 100) && cord(item) > -300) {
+      item.style.transform = `translateX(${cord(item)/10 - 50}px)`
+    }
   })
- 
-  // console.log(v)
 
-  // v.style.transform = `translateX(${(-y)/10}}px)`
+  // let prevY = window.scrollY;
+  // console.log(prevY)
+  // let curY = window.scrollY;
+  // let y = window.scrollY;
+  // let dy;
+  // curY = window.scrollY;
+  // dy = curY - prevY;
+  // y = Math.abs(dy) < 1 ? curY : y + dy * smoothCoef;
+  // prevY = y;
+  // console.log(y)
+})
+
+// showBlocks()
+
+// Скрытие блоков
+
+
+
+
+
+
+// // console.log(window.innerHeight/15)
+
+// function loop(now) {
+//   curY = window.scrollY;
+//   dy = curY - prevY;
+//   y = Math.abs(dy) < 1 ? curY : y + dy * smoothCoef;
+//   prevY = y;
+
+  
+
+  
+//   smoothScroll.style.transform = `translate3d(0,${-y}px,0)`;
+  
+ 
+//   // console.log(v)
+
+//   // v.style.transform = `translateX(${(-y)/10}}px)`
     
-  requestAnimationFrame(loop);
-}
-requestAnimationFrame(loop);
+//   requestAnimationFrame(loop);
+// }
+// requestAnimationFrame(loop);
 
 function offset(el) {
   const rect = el.getBoundingClientRect()
@@ -138,55 +284,65 @@ window.addEventListener("scroll", (e) => {
   
 })
 
-// Кнопка "Стрелка вниз"
+// Скролл до блоков
 
-const arrowBanner = document.querySelector(".banner__arrow-bottom")
-const aboutLink = document.querySelector(".about-link")
-const productsLink = document.querySelector(".products-link")
-const about = document.getElementById("about")
-const products = document.getElementById("products")
+const scrollLink = () => {
+  const arrowBanner = document.querySelector(".banner__arrow-bottom")
+  const aboutLink = document.querySelector(".about-link")
+  const servicesLink = document.querySelector(".services-link")
+  // const productsLink = document.querySelector(".products-link")
+  const about = document.getElementById("about")
+  const services = document.getElementById("services")
+  // const products = document.getElementById("products")
 
-const scrollToFunc = (link, block) => {
-  link.addEventListener("click", () => {
-    setTimeout(() => {
-      window.scrollTo(0, cord(block) - 94)
-    },0)
-  })
+  const elemOffsetY = (elem) => {
+    let box = elem.getBoundingClientRect()
+
+    return box.top + window.pageYOffset
+  }
+
+  const scrollToFunc = (link, block) => {
+    link.addEventListener("click", () => {
+      setTimeout(() => {
+        window.scrollTo(0, elemOffsetY(block) - 90)
+      },0)
+    })
+  }
+
+  scrollToFunc(arrowBanner, about)
+  scrollToFunc(aboutLink, about)
+  scrollToFunc(servicesLink, services)
 }
 
-console.log(about.offsetHeight)
-console.log(cord(about))
+scrollLink()
 
-scrollToFunc(arrowBanner, about)
-
-
-aboutLink.addEventListener("click", () => {
-  setTimeout(() => {
-    // window.scrollTo(0, 0)
-    window.scrollBy(0, -cord(about) + about.offsetHeight)
-  },0)
-})
+// aboutLink.addEventListener("click", () => {
+//   setTimeout(() => {
+//     // window.scrollTo(0, 0)
+//     window.scrollBy(0, -cord(about) + about.offsetHeight)
+//   },0)
+// })
 
 // Анимация "Наши услуги"
 
-const services = document.querySelector('.services__items')
+const servicesItems = document.querySelector('.services__items')
 
 
 
 const observerServices = new MutationObserver(function(mutations) {
   let i = 0
-  services.children[i].classList.add("_active")
+  servicesItems.children[i].classList.add("_active")
   const servicesShow = setInterval(() => {
-    if (services.lastElementChild.classList.contains("_active")) {
+    if (servicesItems.lastElementChild.classList.contains("_active")) {
       clearInterval(servicesShow)
     } else {
       i++
-      services.children[i].classList.add("_active")
+      servicesItems.children[i].classList.add("_active")
     }
   }, 1000)
 });
 
-observerServices.observe(services, obs);
+observerServices.observe(servicesItems, obs);
 
 
 const servicesLeft = document.querySelector('.services__left')
@@ -253,25 +409,44 @@ for (let i = 0; i < acc.length; i++) {
   });
 }
 
-
 // Появление карточек преимуществ
 
-const cardsWrapper = document.querySelector('.benefits__cards')
+const cardsWrapperFirst = document.querySelector('.cards-first')
 
-const observerCards = new MutationObserver(function(mutations) {
+const observerCardsFirst = new MutationObserver(function(mutations) {
   // console.log(mutations);
   let i = 0
   const cardsShow = setInterval(() => {
-    if (cardsWrapper.lastElementChild.classList.contains("_active")) {
+    if (cardsWrapperFirst.lastElementChild.classList.contains("_active")) {
       clearInterval(cardsShow)
     } else {
-      cardsWrapper.children[i].classList.add("_active")
+      cardsWrapperFirst.children[i].classList.add("_active")
       i++
     }
   }, 400)
 });
 
-observerCards.observe(cardsWrapper, obs);
+observerCardsFirst.observe(cardsWrapperFirst, obs);
+
+
+const cardsWrapperSecond = document.querySelector('.cards-second')
+
+const observerCardsSecond = new MutationObserver(function(mutations) {
+  // console.log(mutations);
+  let i = 0
+  cardsWrapperSecond.children[i].classList.add("_active")
+  i++
+  const cardsShow = setInterval(() => {
+    if (cardsWrapperSecond.lastElementChild.classList.contains("_active")) {
+      clearInterval(cardsShow)
+    } else {
+      cardsWrapperSecond.children[i].classList.add("_active")
+      i++
+    }
+  }, 400)
+});
+
+observerCardsSecond.observe(cardsWrapperSecond, obs);
 
 // Наведение на 3D карточки
 
@@ -286,6 +461,56 @@ VanillaTilt.init(cards, {
   transition:  true,
   speed: 3000,
 })
+
+// Кнопка вверх
+
+const btnUp = {
+  el: document.querySelector('.btn-up'),
+  scrolling: false,
+  show() {
+    if (this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
+      this.el.classList.remove('btn-up_hide');
+      this.el.classList.add('btn-up_hiding');
+      window.setTimeout(() => {
+        this.el.classList.remove('btn-up_hiding');
+      }, 300);
+    }
+  },
+  hide() {
+    if (!this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
+      this.el.classList.add('btn-up_hiding');
+      window.setTimeout(() => {
+        this.el.classList.add('btn-up_hide');
+        this.el.classList.remove('btn-up_hiding');
+      }, 300);
+    }
+  },
+  addEventListener() {
+    window.addEventListener('scroll', () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      if (this.scrolling && scrollY > 0) {
+        return;
+      }
+      this.scrolling = false;
+      if (scrollY > 400) {
+        this.show();
+      } else {
+        this.hide();
+      }
+    });
+    document.querySelector('.btn-up').onclick = () => {
+      this.scrolling = true;
+      this.hide();
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+  }
+}
+
+btnUp.addEventListener();
 
 // console.log(cards)
 
@@ -366,6 +591,11 @@ var swiper1 = new Swiper(".mySwiper1", {
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: "auto",
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
   coverflowEffect: {
     rotate: 50,
     stretch: 0,
@@ -384,12 +614,13 @@ var swiper1 = new Swiper(".mySwiper1", {
 });
 
 var swiper2 = new Swiper(".mySwiper2", {
-  slidesPerView: 1.5,
+  slidesPerView: 1.8,
   centeredSlides: true,
   spaceBetween: 30,
   autoplay: {
     delay: 5000,
     disableOnInteraction: false,
+    pauseOnMouseEnter: true,
   },
   pagination: {
     el: ".swiper-pagination",
@@ -410,6 +641,7 @@ var swiper3 = new Swiper(".mySwiper3", {
   autoplay: {
     delay: 5000,
     disableOnInteraction: false,
+    pauseOnMouseEnter: true,
   },
   navigation: {
     nextEl: ".partners .swiper-button-next",
@@ -417,3 +649,5 @@ var swiper3 = new Swiper(".mySwiper3", {
   },
   loop: true
 });
+
+
